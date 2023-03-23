6a9344934df517716ebfdc37859d2452c7810653
mahalakshmichandrasekar <sm2mahaqa@gmail.com>
2023-03-23 11:08:09 -0400

    commit-changed-url

--- src/components/complaints.tsx
+++ src/components/complaints.tsx
@@ -1,71 +1,71 @@
-import React, { useState } from 'react';
+  import React, { useState } from 'react';
 
-type ComplaintForm = {
-  description: string,
-};
-
-export type Complaint = {
-  id: number,
-  form: ComplaintForm,
-};
-
-const ComplaintsPage = () => {
-  const [form, setForm] = useState<ComplaintForm>({ description: '' });
-  const [complaints, setComplaints] = useState<Complaint[]>([]);
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const { name, value } = e.target;
-    setForm((prevForm) => ({ ...prevForm, [name]: value }));
+  type ComplaintForm = {
+    description: string,
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    setComplaints((prevComplaints) => [...prevComplaints, { id: Date.now(), form: form, },]);
-    setForm({
-      description: '',
-    });
+  export type Complaint = {
+    id: number,
+    form: ComplaintForm,
   };
 
-  return (
+  const ComplaintsPage = () => {
+    const [form, setForm] = useState<ComplaintForm>({ description: '' });
+    const [complaints, setComplaints] = useState<Complaint[]>([]);
 
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', border: '1px solid #ccc', padding: '20px' }}>
+    const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+      const { name, value } = e.target;
+      setForm((prevForm) => ({ ...prevForm, [name]: value }));
+    };
 
-      <div
-        onSubmit={handleSubmit}
-        style={{ maxWidth: '400px', width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '10px' }}
-      >
-        <div>
-          <label htmlFor="description">Description</label>
-          <textarea name="description" value={form.description} onChange={handleInputChange} rows={10} cols={50}></textarea>
+    const handleSubmit = (e: React.FormEvent<HTMLDivElement>) => {
+      e.preventDefault();
+      setComplaints((prevComplaints) => [...prevComplaints, { id: Date.now(), form: form, },]);
+      setForm({
+        description: '',
+      });
+    };
 
+    return (
+
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', border: '1px solid #ccc', padding: '20px' }}>
+
+        <div
+          onSubmit={handleSubmit}
+          style={{ maxWidth: '400px', width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '10px' }}
+        >
+          <div>
+            <label htmlFor="description">Description</label>
+            <textarea name="description" value={form.description} onChange={handleInputChange} rows={10} cols={50}></textarea>
+
+          </div>
+          <button type="submit">Add Complaint</button>
         </div>
-        <button type="submit">Add Complaint</button>
-      </div>
 
-      <div style={{ width: '100%', overflowX: 'auto', marginTop: '20px' }}>
-        <table style={{ width: '100%', borderCollapse: 'collapse' }}>
-          <thead>
-            <tr style={{ backgroundColor: 'lightblue' }}>
-              <th style={{ border: '1px solid #ccc', padding: '10px', borderColor: 'black' }}>ID</th>
-              <th style={{ border: '1px solid #ccc', padding: '10px', borderColor: 'black' }}>Description</th>
-            </tr>
-          </thead>
-          <tbody>
-            {complaints.map((complaint) => (
-              <tr key={complaint.id}>
-                <td style={{ border: '1px solid #ccc', padding: '10px', borderColor: 'black' }}>{complaint.id}</td>
-                <td style={{ border: '1px solid #ccc', padding: '10px', borderColor: 'black' }}>{complaint.form.description}</td>
+        <div style={{ width: '100%', overflowX: 'auto', marginTop: '20px' }}>
+          <table style={{ width: '100%', borderCollapse: 'collapse' }}>
+            <thead>
+              <tr style={{ backgroundColor: 'lightblue' }}>
+                <th style={{ border: '1px solid #ccc', padding: '10px', borderColor: 'black' }}>ID</th>
+                <th style={{ border: '1px solid #ccc', padding: '10px', borderColor: 'black' }}>Description</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {complaints.map((complaint) => (
+                <tr key={complaint.id}>
+                  <td style={{ border: '1px solid #ccc', padding: '10px', borderColor: 'black' }}>{complaint.id}</td>
+                  <td style={{ border: '1px solid #ccc', padding: '10px', borderColor: 'black' }}>{complaint.form.description}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
+        <a href="/">Home</a>
       </div>
 
-      <a href="/">Home</a>
-    </div>
+    );
 
-  );
+  };
 
-};
-
-export default ComplaintsPage;
+  export default ComplaintsPage;
--- src/pages/signinPage.tsx
+++ src/pages/signinPage.tsx
@@ -1,61 +1,65 @@
-import { useEffect, useState } from 'react';
-import { Login, userAuthentication } from '../api/requests';
+  import { useEffect, useState } from 'react';
+  import { Login, userAuthentication } from '../api/requests';
 
-interface SignInProps {
-  onLogin: (role: 'Constituent' | 'Council' | 'Unregistered User', user: any) => void;
-}
+  interface SignInProps {
+    onLogin: (role: 'Constituent' | 'Council' | 'Unregistered User', user: any) => void;
+  }
 
-export function SignIn(props: SignInProps) {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState("");
-    const [newLogin, setNewLogin] = useState<Login>({ userName: "", password: "" });
-  
-    useEffect(() => {
-      setNewLogin({ userName: username, password: password });
-    }, [username, password]);
-  
-    const handleSignIn = async () => {
-      try {
-        const response = await userAuthentication(newLogin);
-        if (response) {
-          // handle success response here
-          console.log(response);
-          localStorage.setItem("user", JSON.stringify(response)); // store response in local storage
-          props.onLogin(response.role as "Constituent" | "Council", response);
-        } else {
-          setError("Invalid login");
-        }
-      } catch (error) {
-        setError("Invalid login credentials");
-      }
-    };
+  export function SignIn(props: SignInProps) {
+      const [username, setUsername] = useState("");
+      const [password, setPassword] = useState("");
+      const [error, setError] = useState("");
+      const [newLogin, setNewLogin] = useState<Login>({ userName: "", password: "" });
     
-      const handleGuestLogin = () => {
-        const user = { userName: 'Guest', role: 'Unregistered User' }; // sample user object
-        props.onLogin('Unregistered User', user);
+      useEffect(() => {
+        setNewLogin({ userName: username, password: password });
+      }, [username, password]);
+    
+      const handleSignIn = async () => {
+        try {
+          const response = await userAuthentication(newLogin);
+          if (response) {
+            // handle success response here
+            console.log(response);
+            localStorage.setItem("user", JSON.stringify(response)); // store response in local storage
+            props.onLogin(response.role as "Constituent" | "Council", response);
+          } else {
+            setError("Invalid login");
+          }
+        } catch (error) {
+          setError("Invalid login credentials");
+        }
       };
       
-    
-    return <>
-     
+        const handleGuestLogin = () => {
+          const user = { userName: 'Guest', role: 'Unregistered User' }; // sample user object
+          props.onLogin('Unregistered User', user);
+        };
+        
+      
+      return <>
 
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh', backgroundColor: '#f5f5f5' }}>
+  <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh', backgroundColor: '#f5f5f5' }}>
     <h1>Welcome to Attleboro City Council</h1>
-      <h2 style={{ marginBottom: '1rem' }}>Sign In</h2>
-      <div onSubmit={(e) => e.preventDefault()} style={{ display: 'flex', flexDirection: 'column' }}>
-        <label style={{ marginBottom: '0.5rem' }}>
-          Username:
-          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} style={{ marginLeft: '0.5rem' }} />
-        </label>
-        <label style={{ marginBottom: '0.5rem' }}>
-          Password:
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} style={{ marginLeft: '0.5rem' }} />
-        </label>
+    <h2 style={{ marginBottom: '1rem' }}>Sign In</h2>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', border: '1px solid #ccc', padding: '1rem', borderRadius: '0.25rem' }} onSubmit={(e) => e.preventDefault()} >
+      <label style={{ marginBottom: '0.5rem' }}>
+        Username:
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} style={{ marginLeft: '0.5rem' }} />
+      </label>
+      <label style={{ marginBottom: '0.5rem' }}>
+        Password:
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} style={{ marginLeft: '0.5rem' }} />
+      </label>
+      <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
+        <button onClick={handleSignIn} style={{ marginRight: '0.5rem', padding: '0.5rem 1rem', backgroundColor: '#007bff', color: 'white', border: 'none', borderRadius: '0.25rem', cursor: 'pointer' }}>Sign In</button>
       </div>
-      <button onClick={handleSignIn} style={{ marginTop: '1rem', padding: '0.5rem 1rem', backgroundColor: '#007bff', color: 'white', border: 'none', borderRadius: '0.25rem', cursor: 'pointer' }}>Sign In</button>
+      <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
       <a href="#" onClick={handleGuestLogin} style={{ marginTop: '0.5rem', fontSize: '0.875rem', textDecoration: 'none', color: '#007bff', cursor: 'pointer' }}>Continue as Guest</a>
-      {error && <p className="error" style={{ marginTop: '1rem', color: 'red' }}>{error}</p>}
+      </div>
     </div>
+    {error && <p className="error" style={{ marginTop: '1rem', color: 'red' }}>{error}</p>}
+  </div>
+
     </>
-};
+  };
